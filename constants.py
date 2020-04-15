550f0fd7a4b40bef8e893c670e81f2627f0805bc
igor-simoes <igorsimoes@locus.software>
2020-04-15 13:01:54 -0300

    Add constants.py

---
+++ constants.py
@@ -0,0 +1,9 @@
+# github api params
+# q -> query
+# per_page -> items fetched per page
+# language -> repo language
+# query+in:attrs -> returns repos if the query is in repo attrs
+API_GITHUB_URL = "https://api.github.com/search/repositories?per_page=100&"
+JS_LANG_URL = f"q=language:javascript"
+JS_IN_NAME_AND_DESC_URL = f"q=language:javascript&javascript+in:name,description"
+ENGINE_IN_NAME_AND_DESC_URL = f"q=engine+in:name,description"
