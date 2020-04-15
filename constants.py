b6afdfbccf2f8f44c503823e984f172a2070c489
igor-simoes <igorsimoes@locus.software>
2020-04-15 13:35:32 -0300

    Add JS_ENGINE_URL in constants.py

--- constants.py
+++ constants.py
@@ -4,8 +4,9 @@
 # language -> repo language
 # query+in:attrs -> returns repos if the query is in repo attrs
 API_GITHUB_URL = "https://api.github.com/search/repositories?per_page=100&"
-JS_LANG_URL = f"q=language:javascript"
+JS_LANG_URL = f"{API_GITHUB_URL}q=language:javascript"
+JS_ENGINE_URL = f"{API_GITHUB_URL}q=javascript-engine"
 JS_IN_NAME_AND_DESC_URL = (
-    f"q=language:javascript&javascript+in:name,description"
+    f"{API_GITHUB_URL}q=language:javascript&javascript+in:name,description"
 )
-ENGINE_IN_NAME_AND_DESC_URL = f"q=engine+in:name,description"
+ENGINE_IN_NAME_AND_DESC_URL = f"{API_GITHUB_URL}q=engine+in:name,description"
